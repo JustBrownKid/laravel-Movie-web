55ac80878e70bbc4504bd1ebecd01ae8b61126a3
JustBronwKid <larakid8080@gmail.com>
2025-04-08 16:02:52 +0630

    Admin Middleware Add To Admin Rout

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
-
+namespace App\Http\Middleware;
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CastController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\CategoriesController;
 
+
 // Public Routes
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+// noaccess rout 
+Route::get('/noaccess', function () {
+    return view('noaccess');
+})->name('youeNotAdmin');
+
 
 Route::get('/dashboard', [MovieController::class, 'index'])
     ->middleware(['auth', 'verified'])
@@ -23,27 +30,29 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
-    // Admin Panel
-    Route::get('/admin/penal', [MovieController::class, 'admin'])->name('admin.penal');
-    Route::get('/admin/{id}/detail', [MovieController::class, 'adminDetail'])->name('admin.detail');
+    // Admin middelware
+    Route::middleware(['admin'])->group(function () {
+        Route::get('/admin/penal', [MovieController::class, 'admin'])->name('admin.penal');
+        Route::get('/admin/{id}/detail', [MovieController::class, 'adminDetail'])->name('admin.detail');
+        Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+        Route::put('/movies/{id}', [MovieController::class, 'update'])->name('movies.update');
+        Route::delete('/movies/{id}', [MovieController::class, 'destroy'])->name('movies.destroy');
+        // Casts selectin
+        Route::post('/movies/create/actor', [CastController::class, 'createActor'])->name('cast.store');
+        Route::put('/casts/{id}', [CastController::class, 'update'])->name('casts.update');
+        Route::delete('/casts/{id}', [CastController::class, 'destroy'])->name('casts.destroy');
 
-    // Movies
-    Route::get('/movies/create', [MovieController::class, 'create'])->name('movies.create');
+        // Categories (Types)
+        Route::post('/movies/create/type', [CategoriesController::class, 'createType'])->name('type.store');
+        Route::put('/type/{id}', [CategoriesController::class, 'update'])->name('type.update');
+        Route::delete('/type/{id}', [CategoriesController::class, 'destroy'])->name('type.destroy');
+    });
+    
+
     Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
     Route::get('/movies/{id}/play', [MovieController::class, 'play'])->name('movies.play');
-    Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
-    Route::put('/movies/{id}', [MovieController::class, 'update'])->name('movies.update');
-    Route::delete('/movies/{id}', [MovieController::class, 'destroy'])->name('movies.destroy');
 
-    // Casts
-    Route::post('/movies/create/actor', [CastController::class, 'createActor'])->name('cast.store');
-    Route::put('/casts/{id}', [CastController::class, 'update'])->name('casts.update');
-    Route::delete('/casts/{id}', [CastController::class, 'destroy'])->name('casts.destroy');
 
-    // Categories (Types)
-    Route::post('/movies/create/type', [CategoriesController::class, 'createType'])->name('type.store');
-    Route::put('/type/{id}', [CategoriesController::class, 'update'])->name('type.update');
-    Route::delete('/type/{id}', [CategoriesController::class, 'destroy'])->name('type.destroy');
 });
 
 require __DIR__.'/auth.php';
