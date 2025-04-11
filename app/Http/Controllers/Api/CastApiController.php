3ada588dd18f6ec90beea75990e0180d41e74361
JustBronwKid <larakid8080@gmail.com>
2025-04-11 23:47:33 +0630

    Cast creat Rout and function by API

--- app/Http/Controllers/Api/CastApiController.php
+++ app/Http/Controllers/Api/CastApiController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
 
 class CastApiController extends Controller
 {
     //
+    public function createActor(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => ['required', Rule::unique('actors', 'name')],
+            'image' => 'nullable|string',
+        ]);
+        Actor::create([
+            'name' => $validated['name'],
+            'image' => $validated['image'] ?? null,
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Cast created successfully!'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,26 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\Api\CastApiController;
 use App\Http\Controllers\Api\MovieApiController;
 
-Route::post('/login',[AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
-     Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-     Route::get('/movies', [MovieApiController::class, 'index']);
-     Route::get('/movies/{id}', [MovieApiController::class, 'show']);
-     
-     Route::middleware('ApiAdmin')->group(function () {
-         Route::post('/movies', [MovieApiController::class, 'store']);
-         Route::put('/movies/{id}', [MovieApiController::class, 'update']);
-         Route::delete('/movies/{id}', [MovieApiController::class, 'destroy']);
-     });
-     
-    });
+    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+    Route::get('/movies', [MovieApiController::class, 'index']);
+    Route::get('/movies/{id}', [MovieApiController::class, 'show']);
 
+    Route::middleware('ApiAdmin')->group(function () {
+        Route::post('/movies', [MovieApiController::class, 'store']);
+        Route::put('/movies/{id}', [MovieApiController::class, 'update']);
+        Route::delete('/movies/{id}', [MovieApiController::class, 'destroy']);
+
+        Route::post('/movies/create/actor', [CastApiController::class, 'createActor'])->name('cast.store');
+        //    Route::put('/casts/{id}', [CastController::class, 'update'])->name('casts.update');
+        //    Route::delete('/casts/{id}', [CastController::class, 'destroy'])->name('casts.destroy');
+
+    });
+});
